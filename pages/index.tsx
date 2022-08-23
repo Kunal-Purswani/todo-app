b8bc99e50b83c3c392966c8ca8a72c044882a6af
Kunal Purswani <kunalpurswani40@gmail.com>
2022-08-23 23:03:23 +0530

    Final commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Input from './../components/Input';
 import Todos from './../components/Todos';
-import { useRouter } from 'next/router';
-import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
 
 export default function Home() {
-  const { data: session } = useSession();
-  const router = useRouter();
-  useEffect(() => {
-    if (session == undefined) {
-      router.push('/auth/signin')
-    }
-  }, [])
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide" >
       <Head>
